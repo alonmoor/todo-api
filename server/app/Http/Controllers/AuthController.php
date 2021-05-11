a483f4912755f8be391fa65bd7d9a1179223510c
alon <alonmor2@gmail.com>
2021-05-11 16:12:33 +0300

    before uploade to github

--- server/app/Http/Controllers/AuthController.php
+++ server/app/Http/Controllers/AuthController.php
@@ -29,7 +29,7 @@ use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 
 
 
-// https://cubettech.com/resources/blog/api-authentication-using-jwt-in-laravel-5/
+//
 
 class AuthController extends Controller
 {
@@ -120,38 +120,43 @@ class AuthController extends Controller
         ];
     }
   //=====================================================================================
-    public function getAuthUser(Request $request)
-      {
-        //  return response()->json(auth()->user());
+    // public function getAuthUser(Request $request)
+    //   {
+    //       return response()->json(auth()->user());
+    //   }
 
-          $user = JWTAuth::parseToken()->authenticate();
-          dd($user);
-      }
+
+
+    // somewhere in your controller
+public function getAuthUser()
+{
+    try {
+        if (! $user = JWTAuth::parseToken()->authenticate()) {
+            return response()->json(['user_not_found'], 404);
+        }
+    } catch (Tymon\JWTAuth\Exceptions\TokenExpiredException $e) {
+        return response()->json(['token_expired'], $e->getStatusCode());
+    } catch (Tymon\JWTAuth\Exceptions\TokenInvalidException $e) {
+        return response()->json(['token_invalid'], $e->getStatusCode());
+    } catch (Tymon\JWTAuth\Exceptions\JWTException $e) {
+        return response()->json(['token_absent'], $e->getStatusCode());
+    }
+    // Token is valid and we have found the user via the sub claim
+    return response()->json(compact('user'));
+}
     //=====================================================================================
   public function getLogUser(Request $request){
-    // $credentials = $request->only('email', 'password');
-    // try {
-    //     // verify the credentials and create a token for the user
-    //     if (! $token = JWTAuth::attempt($credentials)) {
-    //         return response()->json(['error' => 'invalid_credentials'], 401);
-    //     }
-    // } catch (JWTException $e) {
-    //     // something went wrong
-    //     return response()->json(['error' => 'could_not_create_token'], 500);
-    // }
-    // $user = Auth::user();
 
+      $credentials = $request->only('email', 'password');
+      try {
+          // verify the credentials and create a token for the user
+          if (! $token = JWTAuth::attempt($credentials)) {
+              return response()->json(['error' => 'invalid_credentials'], 401);
+          }
+      } catch (JWTException $e) {
+          // something went wrong
+          return response()->json(['error' => 'could_not_create_token'], 500);
+      }
 
-
-    $user = Auth::user(); // Retrieve the currently authenticated user...
-       $id = Auth::id();
-       dd($id);
-       return $id;
-      return $request->user('web');
-    return $request->user('api');
-     return  response()->json(auth()->user());
-     //return Auth::guard('web')->user()->id;
-     //return Auth::user();
-
-  }
-}
+       return Auth::user();
+    }
